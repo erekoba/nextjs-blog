c34929c35987b28c62402fb6944cf693cceb02b5
erekoba <erecasio0302@gmail.com>
2022-09-27 16:14:25 +0900

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Layout, { siteTitle } from "../compoments/Layout";
 import utilStyles from "../styles/utils.module.css";
@@ -5,16 +6,24 @@ import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../compoments/date";
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
